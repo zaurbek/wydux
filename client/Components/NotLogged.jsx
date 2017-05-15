aaf6dc42844352be8510a156e8e3cba5001847e1
Zaurbek <bigz3733@gmail.com>
2017-05-15 17:35:27 +0000

    changed a element on '/'

--- client/Components/NotLogged.jsx
+++ client/Components/NotLogged.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const NotLogged = () => (
   <div className="user">
-    <p className="text-center from-top">Please <a href="/api/login" disabled={true}className="btn btn-xs btn-success login-link">login</a> in order to use all features of this app!</p>
+    <p className="text-center from-top">Please <a href="#" disabled={true}className="btn btn-xs btn-success login-link">login</a> in order to use all features of this app!</p>
   </div>
 );
 
